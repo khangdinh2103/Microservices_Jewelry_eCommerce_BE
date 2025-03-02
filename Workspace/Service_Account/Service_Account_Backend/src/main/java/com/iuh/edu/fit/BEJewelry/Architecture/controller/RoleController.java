8a29162796c8a573095df5dd1402e14968e83bc9
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-03-02 17:18:34 +0700

    done feature update role

--- Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/controller/RoleController.java
+++ Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.iuh.edu.fit.BEJewelry.Architecture.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,21 @@ public class RoleController {
             throw new IdInvalidException("Role với name = " + r.getName() + " đã tồn tại");
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(this.roleService.create(r));
+    }
+
+    @PutMapping("/roles")
+    @ApiMessage("Update a role")
+    public ResponseEntity<Role> update(@Valid @RequestBody Role r) throws IdInvalidException {
+        // check id
+        if (this.roleService.fetchById(r.getId()) == null) {
+            throw new IdInvalidException("Role với id = " + r.getId() + " không tồn tại");
+        }
+
+        // check name
+        if (this.roleService.existByName(r.getName())) {
+            throw new IdInvalidException("Role với name = " + r.getName() + " đã tồn tại");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.roleService.update(r));
     }
 }
--- Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/service/RoleService.java
+++ Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/service/RoleService.java
@@ -1,6 +1,7 @@
 package com.iuh.edu.fit.BEJewelry.Architecture.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -36,5 +37,32 @@ public class RoleService {
         }
 
         return this.roleRepository.save(r);
+    }
+
+    public Role fetchById(long id) {
+        Optional<Role> roleOptional = this.roleRepository.findById(id);
+        if (roleOptional.isPresent())
+            return roleOptional.get();
+        return null;
+    }
+
+    public Role update(Role r) {
+        Role roleDB = this.fetchById(r.getId());
+        // check permissions
+        if (r.getPermissions() != null) {
+            List<Long> reqPermissions = r.getPermissions()
+                    .stream().map(x -> x.getId())
+                    .collect(Collectors.toList());
+
+            List<Permission> dbPermissions = this.permissionRepository.findByIdIn(reqPermissions);
+            r.setPermissions(dbPermissions);
+        }
+
+        roleDB.setName(r.getName());
+        roleDB.setDescription(r.getDescription());
+        roleDB.setActive(r.isActive());
+        roleDB.setPermissions(r.getPermissions());
+        roleDB = this.roleRepository.save(roleDB);
+        return roleDB;
     }
 }

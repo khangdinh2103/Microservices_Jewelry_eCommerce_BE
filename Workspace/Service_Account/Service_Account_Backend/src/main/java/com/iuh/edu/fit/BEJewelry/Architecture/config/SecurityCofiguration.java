2de1a4124654294fb3b29878a8a4f62b0718216f
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-03-02 22:33:35 +0700

    done feature register

--- Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/config/SecurityCofiguration.java
+++ Workspace/Service_Account/Service_Account_Backend/src/main/java/com/iuh/edu/fit/BEJewelry/Architecture/config/SecurityCofiguration.java
@@ -47,7 +47,8 @@ public class SecurityCofiguration {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/users")
+                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh",
+                                        "/api/v1/auth/register")
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())

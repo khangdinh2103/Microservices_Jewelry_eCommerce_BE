1c56aecf3ff46ff0fb61b66284793d37789b9988
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-03-03 18:10:06 +0700

    update BE GetProductItem

--- Workspace/Service_Cart_Order/Service_Cart_Order_Backend/server.js
+++ Workspace/Service_Cart_Order/Service_Cart_Order_Backend/server.js
@@ -1,14 +1,12 @@
-// server.js
 const express = require('express');
 const sequelize = require('./src/config/syncDatabase'); 
 const cartRoutes = require('./src/routers/client/cartRoutes'); 
 const cors = require("cors");
 
 const app = express();
+app.use(cors()); 
 app.use(express.json());
-
 app.use('/api', cartRoutes);
-app.use(cors());
 
 // Chỉ gọi `sequelize.sync()` tại đây
 sequelize.sync({ force: false }) 
@@ -21,4 +19,4 @@ sequelize.sync({ force: false })
     })
     .catch((error) => {
         console.error('Failed to synchronize database:', error);
-    });
+    });  

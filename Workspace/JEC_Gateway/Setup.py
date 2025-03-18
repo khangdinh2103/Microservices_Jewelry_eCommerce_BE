2873c159a4ea86bd5b8c495ddb061a871fb42cea
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-03-18 11:09:10 +0900

    setup

---
+++ Workspace/JEC_Gateway/Setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        # Web framework
+        "fastapi",
+        "starlette",
+
+        # ASGI server
+        "uvicorn",
+
+        # JWT libraries
+        "jwt",
+        "PyJWT",
+
+        # HTTP clients
+        "requests",
+        "httpx",
+
+        # Data validation
+        "pydantic",
+    ]
+)

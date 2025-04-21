9a82f3ee07da409b06657b319a8e4f8527499316
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-04-21 15:08:02 +0900

    delete: gateway

--- Workspace/JEC_Gateway/src/constant/PathConfig.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Path:
-    dir = os.path.dirname(os.path.abspath(__file__))
-    src = os.path.dirname(dir)
-    root = os.path.dirname(src)
-    rsc = os.path.join(root, "rsc")
-    rsc_service_mapping = os.path.join(rsc, "service", "mapping.json")
